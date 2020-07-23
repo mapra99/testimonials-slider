9fee8472e15351a779ec09a92da2730c7e73918a
Miguel Prada <mapra99@gmail.com>
2020-07-23 00:05:24 -0500

    dev server

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,16 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
+  mode: "development",
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  devServer: {
+    contentBase: './dist',
+  },
+  devtool: 'inline-source-map',
   plugins: [
     new HtmlWebpackPlugin({  
       filename: 'index.html',
